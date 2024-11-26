770a7953feb3c85581ad656b5c58203a7140d536
m-ghozi <ghozi286@gmail.com>
2024-11-26 20:13:52 +0700

    remove : nvim-notify 🔔

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -50,13 +50,6 @@ return {
       opts.presets.lsp_doc_border = true
     end,
   },
-
-  {
-    "rcarriga/nvim-notify",
-    opts = {
-      timeout = 5000,
-    },
-  },
   {
     "folke/snacks.nvim",
     opts = {
